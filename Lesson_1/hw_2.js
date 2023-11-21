eca671d9f12cbf5a9bc1102f209a64b0bd873d3f
Nakk-i <nastyabond90@gmail.com>
2023-11-21 17:25:52 +0200

    add conditions to tasks and console.log in task 4;

--- Lesson_1/hw_2.js
+++ Lesson_1/hw_2.js
@@ -1,3 +1,5 @@
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
 let firstName ='Анастасия';
 let middleName = 'Николаевна';
 let lastName = 'Бондаренко';
--- Lesson_1/hw_4.js
+++ Lesson_1/hw_4.js
@@ -1,6 +1,13 @@
+//Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+
 let name = prompt("Name",'');
+console.log(name)
 
 let midName = prompt("MiddleName", '');
+console.log(midName)
 
-let age = prompt("age", '')
+let age = prompt("age", 33)
+console.log(typeof age)
 
